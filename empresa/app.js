4e565148142d74243745b98f0d16f67e28fd5501
sistemarecrutsmento <fabio08dejesusjunior@gmail.com>
2026-08-05 01:26:58 -0300

    Atualiza cache da correção de carregamento

--- empresa/app.js
+++ empresa/app.js
@@ -1242,7 +1242,7 @@ function carregarAnalisarEmbed() {
     frame.removeAttribute('src');
     return;
   }
-  const src = 'analisar.html?id=' + encodeURIComponent(id) + '&embed=1';
+  const src = 'analisar.html?id=' + encodeURIComponent(id) + '&embed=1&v=empresa-analisar-3';
   if (frame.getAttribute('src') !== src) frame.src = src;
 }
 function analisarCandidatura(id) {
